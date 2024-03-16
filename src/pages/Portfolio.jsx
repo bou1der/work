46bda885d861d0566d419ed6477d73dd3857d690
bou1der <krowkin@bk.ru>
2024-03-16 16:37:36 +0300

    carousel

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,39 +1,73 @@
 import ImageFigma from "../components/portfolio/resource/figma.svg"
 import ImageGithub from "../components/portfolio/resource/github.svg"
-import ImageEmpty from "../components/portfolio/resource/img.png"
+import ImageEmpty from "../components/portfolio/resource/img1.png"
 import "../components/portfolio/portfolio.css"
 
 const Portfolio = () =>{
     return(
         <>
-        <div className={"main-info-block main-portfolio-block"}> {/* Проблема тута в height */}
-            <div className="portfolio-work-block ">
-                <img src={ImageEmpty} alt="" className="main-img-work" />
-                <div className="portfolio-links">
-                    <a className="portfolio-link-github" href="#" target="blank"><img src={ImageGithub} alt="" /></a>
-                    <a className="portfolio-link-figma" href="#"  target="blank"><img src={ImageFigma} alt="" /></a>
-                </div>
-                <div className="portfolio-comments">
-                    <div className={"comments"}>
-                        <div className={"portfolio-user-comment"}>
-                            <span><h2>Anonymous</h2><small>DATE_STAMP</small></span>
-                            <div>
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, culpa cumque doloremque eaque enim error eveniet facere, ipsa nam nostrum numquam optio quam quia rem repudiandae sed tempora. Cum deserunt mollitia nam nulla pariatur voluptates.
+        <div className={"main-info-block main-portfolio-block"}>
+            <div className={"test"}>
+                        <div className="portfolio-work-block ">
+                            <img src={ImageEmpty} alt="" className="main-img-work"/>
+                            <div className="portfolio-links">
+                                <a className="portfolio-link-github" href="#" target="blank"><img src={ImageGithub} alt=""/></a>
+                                <a className="portfolio-link-figma" href="#" target="blank"><img src={ImageFigma} alt=""/></a>
                             </div>
-                        </div>
+                            <div className="portfolio-comments">
+                                <div className={"comments"}>
+                                    <div className={"portfolio-user-comment"}>
+                                        <span><h2>Anonymous</h2><small>DATE_STAMP</small></span>
+                                        <p>
+                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, culpa cumque
+                                            doloremque eaque enim error eveniet facere, ipsa nam nostrum numquam optio quam quia
+                                            rem repudiandae sed tempora. Cum deserunt mollitia nam nulla pariatur voluptates.
+                                        </p>
+                                    </div>
 
-                        <div className={"portfolio-user-comment"}>
-                            <span><h2>Anonymous</h2><small>DATE_STAMP</small></span>
-                            <div>
-                                Lorem ipsum dolor sit amet.
+                                    <div className={"portfolio-user-comment"}>
+                                        <span><h2>Anonymous</h2><small>DATE_STAMP</small></span>
+                                        <p>
+                                            Lorem ipsum dolor sit amet.
+                                        </p>
+                                    </div>
+                                </div>
+                                <div className={"portfolio-comments-controls"}>
+                                    <input type="text"/>
+                                    <button>Send</button>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div className={"portfolio-comments-controls"}>
-                        <input type="text"/>
-                        <button>Send</button>
-                    </div>
-                </div>
+                        <div className="portfolio-work-block ">
+                            <img src={ImageEmpty} alt="" className="main-img-work"/>
+                            <div className="portfolio-links">
+                                <a className="portfolio-link-github" href="#" target="blank"><img src={ImageGithub} alt=""/></a>
+                                <a className="portfolio-link-figma" href="#" target="blank"><img src={ImageFigma} alt=""/></a>
+                            </div>
+                            <div className="portfolio-comments">
+                                <div className={"comments"}>
+                                    <div className={"portfolio-user-comment"}>
+                                        <span><h2>Anonymous</h2><small>DATE_STAMP</small></span>
+                                        <p>
+                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, culpa cumque
+                                            doloremque eaque enim error eveniet facere, ipsa nam nostrum numquam optio quam quia
+                                            rem repudiandae sed tempora. Cum deserunt mollitia nam nulla pariatur voluptates.
+                                        </p>
+                                    </div>
+
+                                    <div className={"portfolio-user-comment"}>
+                                        <span><h2>Anonymous</h2><small>DATE_STAMP</small></span>
+                                        <p>
+                                            Lorem ipsum dolor sit amet.
+                                        </p>
+                                    </div>
+                                </div>
+                                <div className={"portfolio-comments-controls"}>
+                                    <input type="text"/>
+                                    <button>Send</button>
+                                </div>
+                            </div>
+                        </div>
             </div>
         </div>
         </>
