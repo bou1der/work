17f929a1e11ed25635345aba5ef8871bd9043530
bou1der <krowkin@bk.ru>
2024-03-16 19:02:38 +0300

    carousel adaptive

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -6,70 +6,228 @@ import "../components/portfolio/portfolio.css"
 const Portfolio = () =>{
     return(
         <>
-        <div className={"main-info-block main-portfolio-block"}>
-            <div className={"test"}>
-                        <div className="portfolio-work-block ">
-                            <img src={ImageEmpty} alt="" className="main-img-work"/>
-                            <div className="portfolio-links">
-                                <a className="portfolio-link-github" href="#" target="blank"><img src={ImageGithub} alt=""/></a>
-                                <a className="portfolio-link-figma" href="#" target="blank"><img src={ImageFigma} alt=""/></a>
-                            </div>
-                            <div className="portfolio-comments">
-                                <div className={"comments"}>
-                                    <div className={"portfolio-user-comment"}>
-                                        <span><h2>Anonymous</h2><small>DATE_STAMP</small></span>
-                                        <p>
-                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, culpa cumque
-                                            doloremque eaque enim error eveniet facere, ipsa nam nostrum numquam optio quam quia
-                                            rem repudiandae sed tempora. Cum deserunt mollitia nam nulla pariatur voluptates.
-                                        </p>
-                                    </div>
+            <div className={"main-info-block main-portfolio-block"}>
+                    <div className={"carousel-out"}>
+                        <div className="carousel">
+                            <div className="portfolio-work-block ">
+                                <img src={ImageEmpty} alt="" className="main-img-work"/>
+                                <div className="portfolio-links">
+                                    <a className="portfolio-link-github" href="#" target="blank"><img src={ImageGithub}
+                                                                                                      alt=""/></a>
+                                    <a className="portfolio-link-figma" href="#" target="blank"><img src={ImageFigma}
+                                                                                                     alt=""/></a>
+                                </div>
+                                <div className="portfolio-comments">
+                                    <div className={"comments"}>
+                                        <div className={"portfolio-user-comment"}>
+                                            <span><h2>Anonymous</h2><small>DATE_STAMP</small></span>
+                                            <p>
+                                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, culpa
+                                                cumque
+                                                doloremque eaque enim error eveniet facere, ipsa nam nostrum numquam
+                                                optio quam
+                                                quia
+                                                rem repudiandae sed tempora. Cum deserunt mollitia nam nulla pariatur
+                                                voluptates.
+                                            </p>
+                                        </div>
 
-                                    <div className={"portfolio-user-comment"}>
-                                        <span><h2>Anonymous</h2><small>DATE_STAMP</small></span>
-                                        <p>
-                                            Lorem ipsum dolor sit amet.
-                                        </p>
+                                        <div className={"portfolio-user-comment"}>
+                                            <span><h2>Anonymous</h2><small>DATE_STAMP</small></span>
+                                            <p>
+                                                Lorem ipsum dolor sit amet.
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <div className={"portfolio-comments-controls"}>
+                                        <input type="text"/>
+                                        <button>Send</button>
                                     </div>
                                 </div>
-                                <div className={"portfolio-comments-controls"}>
-                                    <input type="text"/>
-                                    <button>Send</button>
+                            </div>
+                            <div className="portfolio-work-block ">
+                                <img src={ImageEmpty} alt="" className="main-img-work"/>
+                                <div className="portfolio-links">
+                                    <a className="portfolio-link-github" href="#" target="blank"><img src={ImageGithub}
+                                                                                                      alt=""/></a>
+                                    <a className="portfolio-link-figma" href="#" target="blank"><img src={ImageFigma}
+                                                                                                     alt=""/></a>
+                                </div>
+                                <div className="portfolio-comments">
+                                    <div className={"comments"}>
+                                        <div className={"portfolio-user-comment"}>
+                                            <span><h2>Anonymous</h2><small>DATE_STAMP</small></span>
+                                            <p>
+                                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, culpa
+                                                cumque
+                                                doloremque eaque enim error eveniet facere, ipsa nam nostrum numquam
+                                                optio quam
+                                                quia
+                                                rem repudiandae sed tempora. Cum deserunt mollitia nam nulla pariatur
+                                                voluptates.
+                                            </p>
+                                        </div>
+
+                                        <div className={"portfolio-user-comment"}>
+                                            <span><h2>Anonymous</h2><small>DATE_STAMP</small></span>
+                                            <p>
+                                                Lorem ipsum dolor sit amet.
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <div className={"portfolio-comments-controls"}>
+                                        <input type="text"/>
+                                        <button>Send</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="portfolio-work-block ">
+                                <img src={ImageEmpty} alt="" className="main-img-work"/>
+                                <div className="portfolio-links">
+                                    <a className="portfolio-link-github" href="#" target="blank"><img src={ImageGithub}
+                                                                                                      alt=""/></a>
+                                    <a className="portfolio-link-figma" href="#" target="blank"><img src={ImageFigma}
+                                                                                                     alt=""/></a>
+                                </div>
+                                <div className="portfolio-comments">
+                                    <div className={"comments"}>
+                                        <div className={"portfolio-user-comment"}>
+                                            <span><h2>Anonymous</h2><small>DATE_STAMP</small></span>
+                                            <p>
+                                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, culpa
+                                                cumque
+                                                doloremque eaque enim error eveniet facere, ipsa nam nostrum numquam
+                                                optio quam
+                                                quia
+                                                rem repudiandae sed tempora. Cum deserunt mollitia nam nulla pariatur
+                                                voluptates.
+                                            </p>
+                                        </div>
+
+                                        <div className={"portfolio-user-comment"}>
+                                            <span><h2>Anonymous</h2><small>DATE_STAMP</small></span>
+                                            <p>
+                                                Lorem ipsum dolor sit amet.
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <div className={"portfolio-comments-controls"}>
+                                        <input type="text"/>
+                                        <button>Send</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="portfolio-work-block ">
+                                <img src={ImageEmpty} alt="" className="main-img-work"/>
+                                <div className="portfolio-links">
+                                    <a className="portfolio-link-github" href="#" target="blank"><img src={ImageGithub}
+                                                                                                      alt=""/></a>
+                                    <a className="portfolio-link-figma" href="#" target="blank"><img src={ImageFigma}
+                                                                                                     alt=""/></a>
+                                </div>
+                                <div className="portfolio-comments">
+                                    <div className={"comments"}>
+                                        <div className={"portfolio-user-comment"}>
+                                            <span><h2>Anonymous</h2><small>DATE_STAMP</small></span>
+                                            <p>
+                                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, culpa
+                                                cumque
+                                                doloremque eaque enim error eveniet facere, ipsa nam nostrum numquam
+                                                optio quam
+                                                quia
+                                                rem repudiandae sed tempora. Cum deserunt mollitia nam nulla pariatur
+                                                voluptates.
+                                            </p>
+                                        </div>
+
+                                        <div className={"portfolio-user-comment"}>
+                                            <span><h2>Anonymous</h2><small>DATE_STAMP</small></span>
+                                            <p>
+                                                Lorem ipsum dolor sit amet.
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <div className={"portfolio-comments-controls"}>
+                                        <input type="text"/>
+                                        <button>Send</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="portfolio-work-block ">
+                                <img src={ImageEmpty} alt="" className="main-img-work"/>
+                                <div className="portfolio-links">
+                                    <a className="portfolio-link-github" href="#" target="blank"><img src={ImageGithub}
+                                                                                                      alt=""/></a>
+                                    <a className="portfolio-link-figma" href="#" target="blank"><img src={ImageFigma}
+                                                                                                     alt=""/></a>
+                                </div>
+                                <div className="portfolio-comments">
+                                    <div className={"comments"}>
+                                        <div className={"portfolio-user-comment"}>
+                                            <span><h2>Anonymous</h2><small>DATE_STAMP</small></span>
+                                            <p>
+                                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, culpa
+                                                cumque
+                                                doloremque eaque enim error eveniet facere, ipsa nam nostrum numquam
+                                                optio quam
+                                                quia
+                                                rem repudiandae sed tempora. Cum deserunt mollitia nam nulla pariatur
+                                                voluptates.
+                                            </p>
+                                        </div>
+
+                                        <div className={"portfolio-user-comment"}>
+                                            <span><h2>Anonymous</h2><small>DATE_STAMP</small></span>
+                                            <p>
+                                                Lorem ipsum dolor sit amet.
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <div className={"portfolio-comments-controls"}>
+                                        <input type="text"/>
+                                        <button>Send</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="portfolio-work-block ">
+                                <img src={ImageEmpty} alt="" className="main-img-work"/>
+                                <div className="portfolio-links">
+                                    <a className="portfolio-link-github" href="#" target="blank"><img src={ImageGithub}
+                                                                                                      alt=""/></a>
+                                    <a className="portfolio-link-figma" href="#" target="blank"><img src={ImageFigma}
+                                                                                                     alt=""/></a>
+                                </div>
+                                <div className="portfolio-comments">
+                                    <div className={"comments"}>
+                                        <div className={"portfolio-user-comment"}>
+                                            <span><h2>Anonymous</h2><small>DATE_STAMP</small></span>
+                                            <p>
+                                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, culpa
+                                                cumque
+                                                doloremque eaque enim error eveniet facere, ipsa nam nostrum numquam
+                                                optio quam
+                                                quia
+                                                rem repudiandae sed tempora. Cum deserunt mollitia nam nulla pariatur
+                                                voluptates.
+                                            </p>
+                                        </div>
+
+                                        <div className={"portfolio-user-comment"}>
+                                            <span><h2>Anonymous</h2><small>DATE_STAMP</small></span>
+                                            <p>
+                                                Lorem ipsum dolor sit amet.
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <div className={"portfolio-comments-controls"}>
+                                        <input type="text"/>
+                                        <button>Send</button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                        <div className="portfolio-work-block ">
-                            <img src={ImageEmpty} alt="" className="main-img-work"/>
-                            <div className="portfolio-links">
-                                <a className="portfolio-link-github" href="#" target="blank"><img src={ImageGithub} alt=""/></a>
-                                <a className="portfolio-link-figma" href="#" target="blank"><img src={ImageFigma} alt=""/></a>
-                            </div>
-                            <div className="portfolio-comments">
-                                <div className={"comments"}>
-                                    <div className={"portfolio-user-comment"}>
-                                        <span><h2>Anonymous</h2><small>DATE_STAMP</small></span>
-                                        <p>
-                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, culpa cumque
-                                            doloremque eaque enim error eveniet facere, ipsa nam nostrum numquam optio quam quia
-                                            rem repudiandae sed tempora. Cum deserunt mollitia nam nulla pariatur voluptates.
-                                        </p>
-                                    </div>
-
-                                    <div className={"portfolio-user-comment"}>
-                                        <span><h2>Anonymous</h2><small>DATE_STAMP</small></span>
-                                        <p>
-                                            Lorem ipsum dolor sit amet.
-                                        </p>
-                                    </div>
-                                </div>
-                                <div className={"portfolio-comments-controls"}>
-                                    <input type="text"/>
-                                    <button>Send</button>
-                                </div>
-                            </div>
-                        </div>
+                    </div>
             </div>
-        </div>
         </>
     )
 }
