de3c76b03e901f3b68761e46e291cf3cf0ad9a42
bou1der <krowkin@bk.ru>
2024-03-31 14:53:11 +0300

    cardMove

--- src/components/skills/cardMovement.js
+++ src/components/skills/cardMovement.js
@@ -1,15 +1,17 @@
 class CardMovement{
     card;
+    cardReact;
     constructor(id){
         this.card = document.getElementById(`${id}`)
+        this.cardReact = this.card.getBoundingClientRect()
     }
     #mapNumberRange(n, a, b, c, d) {
         return ((n - a) * (d - c)) / (b - a) + c
     }
-    moveCard(el){
+    MoveCard(el){
+        const cardRect = this.cardReact
         const pointerX = el.clientX
         const pointerY = el.clientY
-        const cardRect = this.card.getBoundingClientRect()
         const halfWidth = cardRect.width / 2
         const halfHeight = cardRect.height / 2
         const cardCenterX = cardRect.left + halfWidth
@@ -22,6 +24,10 @@ class CardMovement{
         const rx = this.#mapNumberRange(deltaY, 0, halfWidth, 0, 1)
         const ry = this.#mapNumberRange(deltaX, 0, halfHeight, 0, 1)
         this.card.style.transform = `perspective(400px) rotate3d(${-rx}, ${ry}, 0, ${degree}deg)`
+    }
+    MouseLeave(){
+        this.card.style.transition = "transform 0.15s linear"
+        this.card.style.transform = ``;
     }
 }
 export default CardMovement
--- src/components/skills/skillCard.jsx
+++ src/components/skills/skillCard.jsx
@@ -4,32 +4,29 @@ import {Buffer} from "buffer";
 import CardMovement from "./cardMovement.js"
 const Card = ({id,cardID, setModal,setModalInfo, skill}) =>{
     const [logo, setLogo] = React.useState()
-    const card = React.useRef()
+    const [card,setCard] = React.useState()
 
     React.useEffect(()=>{
         const canv = new Canvas(skill.rgb[0], skill.rgb[1], skill.rgb[2],id)
         canv.Draw(100 - skill.percent,skill.percent)
 
-        new CardMovement(cardID)
+        setCard(new CardMovement(cardID))
 
         const imageData = Buffer.from(skill.image)
         const base64 = imageData.toString('base64')
         setLogo(`data:${skill.image.type};base64,${base64}`)
 
     },[])
-    React.useEffect(()=>{
-       console.log("REPDERING")
-    },[])
 
     return (
         <div className={`main-skill-card card`}
-             ref={card}
              id={`${cardID}`}
              onClick={ (el) => {
-            setModal(true)  
-            setModalInfo({id,name:skill.name,logo:logo,text:skill.text,color:skill.rgb})
-            }}
-             // onMouseMove={(el) => card.moveCard(el)}
+                 setModal(true)
+                 setModalInfo({id,name:skill.name,logo:logo,text:skill.text,color:skill.rgb})
+             }}
+             onMouseMove={(el) => card.MoveCard(el)}
+             onMouseLeave={()=>{card.MouseLeave()}}
         style={{
             background:`linear-gradient(135deg , rgb(${skill.rgb[0]},${skill.rgb[1]},${skill.rgb[2]}) 1% , #6100FF 75%)`
         }}
